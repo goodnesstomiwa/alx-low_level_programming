8845caf92583664a7045075dc5712cc554e191e3
goodnesstomiwa <arinolagoodness@gmail.com>
2023-06-15 11:09:39 -0700

    Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program thats uses puts function to print a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0)
+};
