429c5a903e0a01f43eb9782cdcc6b30097da2616
goodnesstomiwa <arinolagoodness@gmail.com>
2023-07-17 09:59:38 -0700

    A new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,10 @@
-#ifndef _DOG_
-#define _DOG_
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 /**
  * struct dog - Defining a dog attributes
